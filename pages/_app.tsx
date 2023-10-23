29ae5af6491f26bbab5bb9c541b08613d17243a8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-10-23 14:02:41 -0300

    material ui and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import type { AppProps } from 'next/app'
 
+const basictheme = createTheme({
+  palette: {
+    mode : 'dark'
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={basictheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
