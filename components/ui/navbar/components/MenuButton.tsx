c14f13b7651d0a43e8a1ccb8e8a5cffb08525421
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-08 15:36:13 -0300

    update Sidebar

--- components/ui/navbar/components/MenuButton.tsx
+++ components/ui/navbar/components/MenuButton.tsx
@@ -1,11 +1,17 @@
 import { IconButton } from '@mui/material';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import { useContext } from 'react';
+import { UIContext } from '@/context';
 
 export const MenuButton = () => {
+
+    const { openMenuSidebar } = useContext(UIContext);
+
     return (
         <IconButton
             size='large'
             edge="start"
+            onClick={openMenuSidebar}
         >
             <MenuOutlinedIcon color='primary' />
         </IconButton>
--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -1,14 +1,19 @@
 import { Box, Drawer, Typography, Divider } from "@mui/material"
 import { Title } from "./components/Title";
 import { ListItems } from "./components/ListItems";
+import { useContext } from 'react';
+import { UIContext } from "@/context";
 
 
 export const Sidebar = () => {
+
+    const { openSidebar, closeMenuSidebar } = useContext(UIContext)
+
     return (
         <Drawer
             anchor="left"
-            open={false}
-            onClose={() => console.log("cerrando")}
+            open={ openSidebar }
+            onClose={closeMenuSidebar}
         >
             <Box sx={{ width: 250 }}>
                 <Title />
