35afbd820b2a91a163232d212dd432a5916603b6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-14 15:37:29 -0300

    openAddEntry in AddEntry

--- components/ui/AddEntry.tsx
+++ components/ui/AddEntry.tsx
@@ -2,13 +2,19 @@ import { Button } from '@mui/material'
 
 
 import AddIcon from '@mui/icons-material/LibraryAdd';
+import { useContext } from 'react';
+import { UIContext } from '@/context';
 
 export const AddEntry = () => {
+
+    const { openAddEntry } = useContext(UIContext)
+
     return (
         <Button
             variant='outlined'
             fullWidth
             endIcon={<AddIcon />}
+            onClick={openAddEntry}
         >
             Add new entry
         </Button>
--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,20 +1,23 @@
 import { Box } from '@mui/material'
 import { AddEntry } from './AddEntry'
 import { CreateEntry } from './CreateEntry'
+import { useContext } from 'react'
+import { UIContext } from '@/context'
 
 
 
 
 
 export const NewEntry = () => {
+
+  const { isAdding } = useContext(UIContext);
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 1 }}>
 
-<AddEntry/> 
-
-<CreateEntry /> 
-
-
+      {
+        isAdding ? <CreateEntry /> : <AddEntry />
+      }
 
     </Box>
   )
