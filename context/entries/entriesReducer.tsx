3e5fe994cccba0351c035d9bd26c690c93481ef6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-15 20:27:48 -0300

    Update-Entry

--- context/entries/entriesReducer.tsx
+++ context/entries/entriesReducer.tsx
@@ -2,7 +2,7 @@ import { Entry } from "@/interfaces";
 import { EntriesState } from ".";
 
 type ActionType = | { type: '[Entry] Add-Entry', payload: Entry }
-    | { type: 'Update-Entry' }
+    | { type: 'Update-Entry', payload: Entry }
     | { type: 'Delete-Entry' }
 
 export const entriesReducer = (state: EntriesState, action: ActionType): EntriesState => {
@@ -11,7 +11,19 @@ export const entriesReducer = (state: EntriesState, action: ActionType): Entries
         case "[Entry] Add-Entry":
             return {
                 ...state,
-                entries : [ ...state.entries, action.payload ] 
+                entries: [...state.entries, action.payload]
+            }
+
+        case "Update-Entry":
+            return {
+                ...state,
+                entries: state.entries.map(entry => {
+                    if (entry._id === action.payload._id) {
+                        entry.status = action.payload.status;
+                        entry.description = action.payload.description;
+                    }
+                    return entry
+                })
             }
 
         default: return state;
