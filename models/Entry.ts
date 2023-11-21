0631f0b4ec0ba381760ac63b094457c9d9e1ef76
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-21 15:37:15 -0300

    EntrySchema

---
+++ models/Entry.ts
@@ -0,0 +1,21 @@
+import { Entry } from '@/interfaces';
+import mongoose, { Schema, Model } from 'mongoose'
+
+interface IEntry extends Entry {}
+
+const EntrySchema = new Schema({
+    description: { type: String, required: true },
+    createdAt: { type: Number, required: true },
+    status: {
+        type: String,
+        enum: {
+            values: ['pending', 'in-progress', 'finished'],
+            message: '{VALUE} no es un estado permitido'
+        }
+    }
+
+})
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', EntrySchema);
+
+export default EntryModel
