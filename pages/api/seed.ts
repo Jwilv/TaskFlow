a2d942ad9e8b4ae17d7d2482a93adb116eb97265
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-21 14:48:51 -0300

    seed update

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,3 +1,4 @@
+import { db } from "@/database"
 import { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
@@ -6,7 +7,18 @@ type Data = {
 }
 
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    if (process.env.NODE_ENV === 'production') {
+        res.status(401).json({
+            ok: false,
+            message: 'No tiene acceso a este servicio'
+        })
+    }
+
+    await db.connect();
+
+    await db.disconnect();
 
     res.status(200).json({
         ok: true,
