da159b19fd71e73df7352ad9c1af03fa7881143f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-23 16:24:40 -0300

    postEntry

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -6,21 +6,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 type Data =
     | { ok: boolean, message: string }
     | { entries: IEntry[] }
-
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-) {
-
-    switch (req.method) {
-        case 'GET':
-            return getEntries(res);
-
-        default:
-            return res.status(400).json({ ok: false, message: 'Endpoint no existe' });
-    }
-
-}
+    | { entry: IEntry }
 
 const getEntries = async (res: NextApiResponse<Data>) => {
     try {
@@ -39,4 +25,47 @@ const getEntries = async (res: NextApiResponse<Data>) => {
             message: 'Error al obtener las entradas',
         })
     }
+}
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { description = '', status} = req.body;
+
+    const newEntry = new Entry({
+        description,
+        status,
+        createdAt: Date.now(),
+    });
+
+    try {
+        await db.connect();
+        await newEntry.save();
+        await db.disconnect();
+
+        return res.status(201).json({ entry: newEntry });
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            message: 'Error al crear la entrada',
+        });
+    }
+
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    switch (req.method) {
+        case 'GET':
+            return getEntries(res);
+
+        case 'POST':
+            return postEntry(req, res);
+
+        default:
+            return res.status(400).json({ ok: false, message: 'Endpoint no existe' });
+    }
+
 }
