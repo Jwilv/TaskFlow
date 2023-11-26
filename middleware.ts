0a396568471583384a38a3e57fce77dcdae2608d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-26 02:10:25 -0300

    redirect url bad request

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,17 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
 
-    console.log({ req : req.nextUrl});
+    if (req.nextUrl.pathname.startsWith('/api/entries/')) {
+        const id = req.nextUrl.pathname.replace('/api/entries/', '');
+        console.log(id);
+        const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+        if (!checkMongoIDRegExp.test(id)) {
+            const url = req.nextUrl.clone();
+            url.pathname = '/api/bad-request';
+            url.search = `?message=${id} is not a valid MongoID`;
+            return NextResponse.rewrite(url);
+        }
+    }
 
     return NextResponse.next();
 }
